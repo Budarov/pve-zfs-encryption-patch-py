824fa980da817c6d1dbc6cb21ed2f026fe8b390d
Budarov <45418632+Budarov@users.noreply.github.com>
2022-04-19 16:41:17 +0300

    Update to multiversion patcher

--- pve-zfs-patch.py
+++ pve-zfs-patch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import hashlib
 import os, sys
@@ -7,19 +7,12 @@ import subprocess
 import json
 from datetime import date
 import shutil
-
-#------python 3.8+ version--------
-#def md5sum(filename):
-#     with open(os.path.abspath(filename), "rb") as f:
-#         file_hash = hashlib.md5()
-#         while chunk := f.read(8192):
-#             file_hash.update(chunk)
-#     return file_hash.hexdigest()
-#---------------------------------
 	 
 fqdn = socket.getaddrinfo(socket.gethostname(), 0, flags=socket.AI_CANONNAME)[0][3]
 email_user = "root@pam"
+conf_file = sys.path[0]+'/'+'pve-patch.conf'
 
+# Проверям параметры запуска
 noemail = False
 if len(sys.argv) > 2:
 	sys.exit("Script has only one argument: -noemail, exit.")
@@ -29,71 +22,76 @@ elif len(sys.argv) == 2:
 		noemail = True
 	else:
 		sys.exit("Script has only one argument: -noemail, exit.")
-	
-	
+
+# Читаем конфигурационный файл	
+def get_conf(file):
+    if os.path.isfile(file):
+        with open(file,'r') as conf_file:
+            try:
+                data = json.load(conf_file)
+            except json.decoder.JSONDecodeError:
+                sys.exit('Not valid json in file ' + file)
+        conf_file.close()
+        return(data['CONF'])
+    else:
+        sys.exit('Conf file ' + file + ' not found')
+
+# Считаем md5	
 def md5sum(filename, blocksize=65536):
     hash = hashlib.md5()
     with open(filename, "rb") as f:
         for block in iter(lambda: f.read(blocksize), b""):
             hash.update(block)
     return hash.hexdigest()
-	
+
+# Получаем email из PVE
+def GetEmail(email_user):
+	try:
+		user_inf = subprocess.run(["pvesh", "get", "/access/users/" + email_user, "--output-format", "json"], capture_output=True, text=True)
+	except FileNotFoundError:
+		sys.exit("pvesh utility not found, this is PVE system?")
+	if user_inf.stderr != "":
+		sys.exit("pvesh err: " + user_inf.stderr)
+	user_inf = json.loads(user_inf.stdout)
+	return user_inf["email"]
+
+# Отправляем email	
 def SendMail(email: str, Subject: str, Body: str):
     body_str_encoded_to_byte = Body.encode()
     return_stat = subprocess.run(["mail", "-s", Subject, email], input=body_str_encoded_to_byte)
-    print(return_stat) 
+    #print(return_stat) 
 
-try:
-	checksumm_origin = md5sum(sys.path[0] + '/' + "ZFSPoolPlugin.pm.orig")
-except FileNotFoundError:
-	sys.exit("File ZFSPoolPlugin.pm.orig not found, your need copy all files.")
+email = GetEmail(email_user)
 
-try:
-	checksumm_patch = md5sum(sys.path[0] + '/' + "ZFSPoolPlugin.pm.patch")
-except FileNotFoundError:
-	sys.exit("File ZFSPoolPlugin.pm.patch not found, your need copy all files.")
+patches = get_conf(conf_file)
 
-try:
-	checksumm_current = md5sum("/usr/share/perl5/PVE/Storage/ZFSPoolPlugin.pm")
-except FileNotFoundError:
-	sys.exit("File ZFSPoolPlugin.pm not found, this is PVE system?")
+for file in patches:
 
+	try:
+		checksumm_current = md5sum(file['SYSFILE'])
+	except FileNotFoundError:
+		sys.exit("File " + file['SYSFILE'] +" not found, this is PVE system?")
+    
+	print("-------------------------------------")
+	print("File: " + file['SYSFILE'])
 
-try:
-	user_inf = subprocess.run(["pvesh", "get", "/access/users/" + email_user, "--output-format", "json"], capture_output=True, text=True)
-except FileNotFoundError:
-	sys.exit("pvesh utility not found, this is PVE system?")
-if user_inf.stderr != "":
-	sys.exit("pvesh err: " + user_inf.stderr)
-
-user_inf = json.loads(user_inf.stdout)
-email = user_inf["email"]
-
-print("checksumm original file:           " + checksumm_origin)
-print("checksumm current file in system:  " + checksumm_current)
-print("checksumm patch file:              " + checksumm_patch)
-print("------------------------------------------")
-if checksumm_current == checksumm_patch:
-	sys.exit("Patch already applyed. Exit.")
-else:
-	if checksumm_current == checksumm_origin:
+	if checksumm_current in file['PATCHES']:
 		print("Do patching...")
 		today = date.today()
-		print("Backup original ZFSPoolPlugin.pm file. New file name: ZFSPoolPlugin.pm." + today.strftime("%Y-%m-%d"))
-		os.rename('/usr/share/perl5/PVE/Storage/ZFSPoolPlugin.pm', '/usr/share/perl5/PVE/Storage/ZFSPoolPlugin.pm.' + today.strftime("%Y-%m-%d"))
+		print("Backup original " + file['SYSFILE'] +" file. New file name: " + file['SYSFILE'] + "." + today.strftime("%Y-%m-%d"))
+		os.rename(file['SYSFILE'], file['SYSFILE'] + "."  + today.strftime("%Y-%m-%d"))
 		print("Copy patch file.")
-		#cp $(dirname "$0")/ZFSPoolPlugin.pm.patch /usr/share/perl5/PVE/Storage/ZFSPoolPlugin.pm
-		shutil.copy(sys.path[0] + '/' + "ZFSPoolPlugin.pm.patch", "/usr/share/perl5/PVE/Storage/ZFSPoolPlugin.pm")
+		shutil.copy(sys.path[0] + '/' + file['PATCHES'][checksumm_current], file['SYSFILE'])
 		if not noemail:
 			print("Sending email whith info to: " + email)
-			SendMail(email, "ZFS Encryption patch applyed on " + fqdn, "Patch applyed on " + fqdn + " Original ZFSPoolPlugin.pm file rename to: ZFSPoolPlugin.pm." + today.strftime("%Y-%m-%d"))
-		sys.exit("Patch applyed. Exit.")
+			SendMail(email, "Patch applyed on " + fqdn, "Patch applyed on " + fqdn + " Original " + file['SYSFILE'] +" file rename to: " + file['SYSFILE'] + today.strftime("%Y-%m-%d"))
+		print("Patch " + file['SYSFILE'] + " applyed.")
 	else:
+		for md5, path in file['PATCHES'].items():
+			if md5sum(sys.path[0] + '/' + path) == checksumm_current:
+				sys.exit("Patch "+ path + " already applyed.")
 		if not noemail:
-			print("Sending email whith info to: " + email)
-			SendMail(email, "ZFS Encryption patch ERROR on " + fqdn, "Patch can not bee applyed to current version ZFSPoolPlugin.pm")
-		sys.exit("Patch can not bee applyed to current version ZFSPoolPlugin.pm. Exit.")
+			print("Sending email whith error to: " + email)
+			SendMail(email, "Patch ERROR on " + fqdn, "Patch can not bee applyed to current version " + file['SYSFILE'])
+		print("Error: Patch can not bee applyed to current version " + file['SYSFILE'])
 		
-
-
-
